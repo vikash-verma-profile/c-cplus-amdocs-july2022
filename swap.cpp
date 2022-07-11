22936337492ee3ebaa68d7f088e122bb52629a4e
Level-Up-4 <vikash-verma-profile>
2022-07-11 11:23:32 +0530

    add

--- swap.cpp
+++ swap.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 using namespace std;
-void Swap(int a,int b)
+void Swap(int &a,int &b);//signature of the function
+int main()
+{
+    
+    int a,b;
+    cout<<"Enter the value of a and b ";
+    cin>>a>>b;
+    cout<<"Value of a and b before swap :- a="<<a<<" b= "<<b<<"\n";
+    Swap(a,b);
+    cout<<"Value of a and b after swa5p :- a="<<a<<" b= "<<b<<"\n";
+    return 0;
+    
+}
+//Defination of function
+void Swap(int &a,int &b)
 {
     int c;
     c=a;
     a=b;
     b=c;
-    cout<<"Value of a and b are :- a="<<a<<" b= "<<b;
-}
-int main()
-{
-    int a,b;
-    cout<<"Enter the value of a and b";
-    cin>>a>>b;
-    Swap(a,b);
-    return 0;
+    cout<<"Value of a and b in swap function :- a="<<a<<" b= "<<b<<"\n";
 }
