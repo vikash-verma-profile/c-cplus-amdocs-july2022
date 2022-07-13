13633c99edf36be56e78e4397ec3873da7d91bdb
Level-Up-4 <vikash-verma-profile>
2022-07-13 09:50:46 +0530

    added code

---
+++ templateexample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T myMax(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+int main()
+{
+    cout << myMax<int>(3, 7) << endl;
+    cout << myMax<double>(3.0, 7.0) << endl;
+    cout << myMax<int>('g', 'e') << endl;
+    return 0;
+}
